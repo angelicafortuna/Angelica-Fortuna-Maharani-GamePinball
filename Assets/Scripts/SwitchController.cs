6f8ad5ff37898e130f88fc8d7b5ab568ae3ff520
angelicafortuna <angelica.22067@mhs.unesa.ac.id>
2024-09-22 19:52:44 +0700

    Angelica Fortuna Maharani-GamePinball

---
+++ Assets/Scripts/SwitchController.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchController : MonoBehaviour
+{
+    private enum SwitchState
+    {
+        Off,
+        On,
+        Blink
+    }
+
+    public Collider bola;
+    public Material offMaterial;
+    public Material onMaterial;
+    public float score;
+
+    public ScoreManager ScoreManager;
+
+    private SwitchState state;
+    private Renderer renderer;
+
+    private void Start()
+    {
+        renderer = GetComponent<Renderer>();
+
+        Set(false);
+
+        StartCoroutine(BlinkTimerStart(5));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == bola)
+        {
+            Toggle();
+        }
+    }
+
+    private void Set(bool active)
+    {
+        if (active == true)
+        {
+            state = SwitchState.On;
+            renderer.material = onMaterial;
+            StopAllCoroutines();
+        }
+        else
+        {
+            state = SwitchState.Off;
+            renderer.material = offMaterial;
+            StartCoroutine(BlinkTimerStart(5));
+        }
+    }
+
+    private void Toggle()
+    {
+        if (state == SwitchState.On)
+        {
+            Set(false);
+        }
+        else
+        {
+            Set(true);
+        }
+    }
+
+    private IEnumerator Blink(int times)
+    {
+        state = SwitchState.Blink;
+
+        for (int i = 0; 1 < times; i++)
+        {
+            renderer.material = onMaterial;
+            yield return new WaitForSeconds(0.5F);
+            renderer.material = offMaterial;
+            yield return new WaitForSeconds(0.5F);
+        }
+
+        state = SwitchState.Off;
+
+        StartCoroutine(BlinkTimerStart(5));
+    }
+
+    private IEnumerator BlinkTimerStart(float time)
+    {
+        yield return new WaitForSeconds(time);
+        StartCoroutine(Blink(2));
+    }
+}
